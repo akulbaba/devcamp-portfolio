1408de023997fdc6cd3df3dd50a7a1d31f7e170a
A Kulbaba <andreakulbaba@gmail.com>
2018-10-29 21:50:42 +0000

    create seeds file with sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+   Blog.create!(
+       title: "My blog post #{blog}",
+       body: "Cupcake ipsum dolor sit amet dessert. Chocolate bar gummies cotton candy lemon drops lemon drops sugar plum jelly jelly beans. Marzipan dragée macaroon marzipan cheesecake tart. Jujubes cupcake pastry pastry cake sweet ice cream tiramisu. Danish cookie oat cake croissant soufflé. Candy canes marshmallow soufflé chocolate bar chocolate cake cupcake soufflé. Dessert carrot cake jujubes ice cream. Liquorice soufflé caramels chupa chups cookie. Jelly beans lemon drops oat cake."
+       ) 
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+    Skill.create!(
+        title: "Rails #{skill}",
+        percent_utilized: 15
+    )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+   Portfolio.create!(
+    title:  "Portfolio title #{portfolio_item}",
+    subtitle: "My great service",
+    body: "Toffee marzipan bear claw jelly-o pudding sesame snaps soufflé apple pie marzipan. ",
+    main_image: "https://placeholdit.co//i/600x400",
+    thumb_image: "https://placeholdit.co//i/350x200"
+    ) 
+end
+
+puts "9 portfolio items created"
