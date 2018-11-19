fcf528b1d259e7e3fbe03dc63d755b6a27ad6282
A Kulbaba <andreakulbaba@gmail.com>
2018-11-19 17:10:52 +0000

    add nav bar and add default header content

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::Base
     include DeviseWhitelist
     include SetSource
     include CurrentUserConcern
+    include DefaultPageContent
+    
+    
+    before_filter set_title
+    
+
 
 end
